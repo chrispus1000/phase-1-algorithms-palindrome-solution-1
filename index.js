fc2a90dfc7156701edad0ec457bdebae7da9a298
chrispus1000 <minrevac@gmail.com>
2022-06-20 00:28:18 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+function reverse(word) {
+  const wordArray = word.split("");
+  const reversedWordArray = wordArray.reverse();
+  const reversedWord = reversedWordArray.join("");
+  return reversedWord;
+}
 function isPalindrome(word) {
-  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 
 /* 
